c43d9b1d8fd9561a85fd19c1871da136839d9634
Je Min Shin <j.shin555>
2021-05-10 20:10:13 -0500

    Init initial files

---
+++ src/main/java/com/dinosaurleg/Jaehatting/JaehattingApplication.java
@@ -0,0 +1,13 @@
+package com.dinosaurleg.Jaehatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaehattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaehattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinosaurleg/Jaehatting/JaehattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinosaurleg.Jaehatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaehattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
